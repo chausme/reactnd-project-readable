545c5aa8dea833622080dd6f7bb8e914ff761b45
Unknown <chausme@gmail.com>
2017-08-17 11:12:06 +0400

    Add static default view

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,34 +19,126 @@ class App extends Component {
         <main>
           <div className="container">
 
-            <div className="row">
-
-              <div className="col-xs-12">[categories]</div>
-
-            </div>
-
-            <div className="row">
-
-              <div className="col-xs-12">[posts]</div>
+            <section className="row">
 
               <div className="col-xs-12">
-                <div className="row">
-                  <div className="col-sm-4">[single post]</div>
-                  <div className="col-sm-4">[single post]</div>
-                  <div className="col-sm-4">[single post]</div>
-                  <div className="col-sm-4">[single post]</div>
-                  <div className="col-sm-4">[single post]</div>
-                  <div className="col-sm-4">[single post]</div>
+                <ul className="categories">
+                  <li>
+                    <a href="#" className="btn btn-primary">React</a>
+                  </li>
+                  <li>
+                    <a href="#" className="btn btn-primary">Redux</a>
+                  </li>
+                  <li>
+                    <a href="#" className="btn btn-primary">Udacity</a>
+                  </li>
+                </ul>
+              </div>
+
+            </section>
+
+            <section className="row posts">
+
+              <div className="col-xs-12">
+                <div className="sort">
+                  <select>
+                    <option>sortby:</option>
+                    <option>votes</option>
+                    <option>date</option>
+                  </select>
                 </div>
               </div>
 
-            </div>
+              <div className="col-xs-12">
+                <div className="row">
 
-            <div className="row">
+                  <div className="col-sm-12 post">
+                    <a href="#"><h2>Post title</h2></a>
+                    <div className="post-meta">
+                      <span>posted by [author] on [timestamp] in <a href="#">[category name]</a> category</span>
+                      <span>0 comments</span>
+                      <span className="score">
+                        <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
+                        <input type="number" id="8xf0y6ziyjabvozdd253nd" value="2" />
+                      </span>
+                    </div>
+                    <div className="excerpt entry">
+                      [trimmed post content]
+                    </div>
+                  </div>
 
-              <div className="col-xs-12">[create/edit posts]</div>
+                  <div className="col-sm-12 post">
+                    <a href="#"><h2>Post title 2</h2></a>
+                    <div className="post-meta">
+                      <span>posted by [author] on [timestamp] in <a href="#">[category name]</a> category</span>
+                      <span>0 comments</span>
+                      <span className="score">
+                        <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
+                        <input type="number" id="8xf0y6ziyjabvozdd253nd" value="2" />
+                      </span>
+                    </div>
+                    <div className="excerpt entry">
+                      [trimmed post content]
+                    </div>
+                  </div>
 
-            </div>
+                  <div className="col-sm-12 post">
+                    <a href="#"><h2>Post title 3</h2></a>
+                    <div className="post-meta">
+                      <span>posted by [author] on [timestamp] in <a href="#">[category name]</a> category</span>
+                      <span>0 comments</span>
+                      <span className="score">
+                        <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
+                        <input type="number" id="8xf0y6ziyjabvozdd253nd" value="2" />
+                      </span>
+                    </div>
+                    <div className="excerpt entry">
+                      [trimmed post content]
+                    </div>
+                  </div>
+
+                  <div className="col-sm-12 post">
+                    <a href="#"><h2>Post title 4</h2></a>
+                    <div className="post-meta">
+                      <span>posted by [author] on [timestamp] in <a href="#">[category name]</a> category</span>
+                      <span>0 comments</span>
+                      <span className="score">
+                        <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
+                        <input type="number" id="8xf0y6ziyjabvozdd253nd" value="2" />
+                      </span>
+                    </div>
+                    <div className="excerpt entry">
+                      [trimmed post content]
+                    </div>
+                  </div>
+
+                  <div className="col-sm-12 post">
+                    <a href="#"><h2>Post title 5</h2></a>
+                    <div className="post-meta">
+                      <span>posted by [author] on [timestamp] in <a href="#">[category name]</a> category</span>
+                      <span>0 comments</span>
+                      <span className="score">
+                        <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
+                        <input type="number" id="8xf0y6ziyjabvozdd253nd" value="2" />
+                      </span>
+                    </div>
+                    <div className="excerpt entry">
+                      [trimmed post content]
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+            </section>
+
+            <section className="row create-post">
+
+              <div className="col-xs-12">
+                <a href="#" className="btn btn-primary btn-success">Create a new post</a>
+              </div>
+
+            </section>
 
           </div>
         </main>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom'
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <App />,
