cc8e241ff77fdab1517368b38a458a94a78b2687
Unknown <chausme@gmail.com>
2017-08-27 14:38:54 +0400

    Add basic delete post functionality

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -5,11 +5,31 @@ const capitalize = require('capitalize');
 
 class SinglePost extends Component {
 
+  deletePost = (e, post) => {
+
+    fetch('http://localhost:5001/posts/' + post.id, { method: 'DELETE', headers: { 'Authorization': 'dmfR05SBzsxn30' } }).then((data) => {
+      data.text().then((data) => {
+        console.log('post deleted!');
+      })
+    })
+
+    e.preventDefault();
+
+  }
+
   render() {
 
     const post = this.props.location.state.post
 
     console.log(post);
+
+    let timestamp = new Date(post.timestamp);
+
+    let options = {
+        year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit"
+    };
+
+    let postDate = timestamp.toLocaleTimeString("en-us", options);
 
     return(
 
@@ -20,14 +40,14 @@ class SinglePost extends Component {
           <h2>{post.title}</h2>
 
           <div className="post-meta">
-            <span>posted by {post.author} on {post.timestamp} in <Link to={`/${post.category}`}>{capitalize(post.category)}</Link> category</span>
+            <span>posted by {post.author} on {postDate} in <Link to={`/${post.category}`}>{capitalize(post.category)}</Link> category</span>
             <span>0 comments</span>
             <span className="score">
               <label htmlFor="8xf0y6ziyjabvozdd253nd">post score</label>
               <input type="number" id="8xf0y6ziyjabvozdd253nd" defaultValue={post.voteScore} />
             </span>
             <span>
-              <Link to={`/edit`}>Edit</Link> | <Link to={`/delete`}>Delete</Link>
+              <Link to={`/edit`}>Edit</Link> | <a href="" onClick={(e) => this.deletePost(e, post)}>Delete</a>
             </span>
           </div>
 
