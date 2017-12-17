c2603997a666b6be1b2de2b0a7ff49642b9f4073
Unknown <chausme@gmail.com>
2017-12-17 18:11:42 +0400

    Add comments count output

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -46,7 +46,7 @@ class SinglePost extends Component {
 
           <div className="post-meta">
             <span>posted by {post.author} on {postDate} in <Link to={`/${post.category}`}>{capitalize(String(post.category))}</Link> category</span>
-            <span>[#] comments</span>
+            <span>{post.commentCount} comments</span>
             <span className="vote-score">
               <button onClick={() => this.voteClicker(id, 'downVote')} className="btn btn-primary">&darr;</button>
               <label>{post.voteScore}</label>
